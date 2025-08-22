5510e395d2b6315cfd970fc760316de97a12fdf8
hardikupadhyay-dg <hardik.upadhyay@datagrokr.co>
2025-08-22 23:35:42 +0530

    this is the final commit

--- test_calculator.py
+++ test_calculator.py
@@ -3,11 +3,11 @@ from calculator import add, subtract, multiply
 
 class TestCalculator(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(add(3,3),6)
+        self.assertEqual(add(6,1),7)
     def test_subtract(self):
-        self.assertEqual(subtract(5,2),3)
-    def test_add(self):
-        self.assertEqual(multiply(3,6),18)
+        self.assertEqual(subtract(9,4),5)
+    def test_multiply(self):
+        self.assertEqual(multiply(1,5),5)
 
 if __name__=='__main__':
     unittest.main()
